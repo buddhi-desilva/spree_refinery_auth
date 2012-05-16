15dc15a7bd70ae54faffa81fe7315f5aca34913b
Marc Lee <marc@adviso.com.au>
2012-05-16 13:46:25 +1000

    redirecting any user related registrations to spree

--- lib/refinery/application_controller.rb
+++ lib/refinery/application_controller.rb
@@ -77,8 +77,8 @@ module Refinery
     end
 
     def refinery_user_required?
-      if just_installed? and controller_name != 'users'
-        redirect_to refinery.new_refinery_user_registration_path
+      if just_installed? and !controller_name.include?('user')
+        redirect_to spree.signup_path
       end
     end
 
